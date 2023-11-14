78fae02ebd097457f4f5cba01e0612717314918e
MarckValle <marco.vallejo2000@gmail.com>
2023-11-14 15:39:49 -0600

    rueba

--- APIZURI/api/paypal_config.py
+++ APIZURI/api/paypal_config.py
@@ -1,14 +1,14 @@
-from django.http import HttpResponse
-import paypalrestsdk
-from paypalrestsdk import CreditCard
+# from django.http import HttpResponse
+# import paypalrestsdk
+# from paypalrestsdk import CreditCard
 
 
-def payment(request):
-    paypalrestsdk.configure({
-        "mode": "sandbox",  # Cambia a "live" para producción
-        "client_id": "AcSvt8ORvhMoD2E2XNUo33-L5BZGj8W5csa7TEZ_x1kSz5E-e_9XMrb3oWYNx7QeN03Sx6LV_8fRjSMp",
-        "client_secret": "EPPCASPCg53q6wpwWsejpa7BK55JppwLTpbB0sV_RBQ4zGm2UFyFC3MKyuXGCoj9BaDtqUTM2C2Rt2C8"
-    })
+# def payment(request):
+#     paypalrestsdk.configure({
+#         "mode": "sandbox",  # Cambia a "live" para producción
+#         "client_id": "AcSvt8ORvhMoD2E2XNUo33-L5BZGj8W5csa7TEZ_x1kSz5E-e_9XMrb3oWYNx7QeN03Sx6LV_8fRjSMp",
+#         "client_secret": "EPPCASPCg53q6wpwWsejpa7BK55JppwLTpbB0sV_RBQ4zGm2UFyFC3MKyuXGCoj9BaDtqUTM2C2Rt2C8"
+#     })
 
     # credit_card = CreditCard({
     #         "type": "visa",
